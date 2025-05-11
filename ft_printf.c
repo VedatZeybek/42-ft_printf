a0b5e1a95210f335c1f57b0e1308eeb279ed94f9
vedat.zeybek <vedatzeybek20@gmail.com>
2025-05-11 12:30:25 +0300

    m

--- ft_printf.c
+++ ft_printf.c
@@ -23,6 +23,47 @@ void	putnbr(int	num)
 	write(1, &result, 1);
 }
 
+void		putnbru(unsigned int num)
+{
+	char result;
+	if (num > 9)
+	{
+		putnbr(num/10);
+	}
+	result = num % 10 + '0';
+	write(1, &result, 1);
+}
+
+void	ft_print_hex(int	num)
+{
+	char	base[16] = "0123456789abcdef";
+	char	result;
+
+	if (num > 16)
+		ft_print_hex(num / 16);
+	result = base[num % 16];
+	write(1, &result, 1);
+}
+void	ft_print_hex_big(int	num)
+{
+	char	base[16] = "0123456789ABCDEF";
+	char	result;
+
+	if (num > 16)
+		ft_print_hex_big(num / 16);
+	result = base[num % 16];
+	write(1, &result, 1);
+}
+
+void	ft_print_ptr(void *ptr)
+{
+	unsigned long long 	num;
+
+	num = (unsigned long long)ptr;
+	write(1, "0x", 2);
+	ft_print_hex(num);
+}
+
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
@@ -30,7 +71,11 @@ int	ft_printf(const char *format, ...)
 	char	character;
 	char	*string;
 	int		integer;
-	
+	int		hex;
+	int		hex2;
+	unsigned int	u;
+	void	*pointer;
+
 	i = 0;
 	va_start(args, format);
 	while (format[i])
@@ -39,6 +84,7 @@ int	ft_printf(const char *format, ...)
 		{
 			character = va_arg(args, int);
 			write(1, &character, 1);
+			i++;
 			i++;
 		}
 		else if (format[i] == '%' && format[i + 1] == 's')
@@ -49,16 +95,62 @@ int	ft_printf(const char *format, ...)
 				string = va_arg(args, char *);
 			write(1, string, strlen(string));
 			i++;
+			i++;
 		}
 		else if (format[i] == '%' && format[i + 1] == 'd')
 		{
 			integer = va_arg(args, long);
 			putnbr(integer);
 			i++;
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == 'i')
+		{
+			integer = va_arg(args, long);
+			putnbr(integer);
+			i++;
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == 'x')
+		{
+			hex = va_arg(args, long);
+			ft_print_hex(hex);
+			i++;
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == 'X')
+		{
+			hex2 = va_arg(args, long);
+			ft_print_hex_big(hex2);
+			i++;
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == 'p')
+		{
+			pointer = va_arg(args, void *);
+			ft_print_ptr(pointer);
+			i++;
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == 'u')
+		{
+			u = va_arg(args, unsigned int);
+			putnbru(u);
+			i++;
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			character = '%';
+			write(1, &character, 1);
+			i++;
+			i++;
 		}
 		else
+		{
 			write (1, &format[i], 1);
 			i++;
+		}
 	}
 	va_end(args);
 	return (i);
@@ -68,8 +160,26 @@ int	ft_printf(const char *format, ...)
 
 int	main()
 {
-	char character = 'f';
-	int integer = -5420000;
-	char	*string = "selam";
-	ft_printf("%c %s %d ", character, string, integer);
+	// char character = 'f';
+	// int integer = -5420000;
+	// char	*string = "selam";
+	// unsigned int unsigned_int = 4294967295;
+	// int	num = 255;
+	// ft_printf("%c %s %d %u %% \n", character, string, integer, unsigned_int);
+
+	// int num = 320;
+
+    // printf("%%x     : %x\n", num);    // ff
+    // printf("%%X     : %X\n", num);    // FF
+    // printf("%%#x    : %#x\n", num);   // 0xff
+    // printf("%%#X    : %#X\n", num);   // 0XFF
+
+	// ft_printf("num: %x\n", num);
+	// ft_printf("num: %X", num);
+
+	int x = 42;
+	printf("Adres: %p\n", &x);  // Örnek çıktı: 0x7ffeefbff5ac
+	ft_printf("%p", &x);
+
 }
+
